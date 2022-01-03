5cc0c46e12d228d72dd6a3d3afed1143dcd421a8
julialoeschel <juloeschel@gmail.com>
2022-01-03 13:16:50 +0100

    toggle arrow

--- scripts/more.js
+++ scripts/more.js
@@ -2,7 +2,6 @@ const moreArrow = document.querySelector("[data-js=more-link]");
 
 export function toggleMore() {
   moreArrow.addEventListener("click", () => {
-    console.log("hi");
-    moreArrow.classList.toggle("bg-red-600");
+    moreArrow.classList.toggle("bg-red-800");
   });
 }
